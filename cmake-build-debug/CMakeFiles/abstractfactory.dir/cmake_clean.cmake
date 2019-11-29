8a7e09889d338439bc0f71f300c4780ec6c9aa3f
Taylor <tlr.laan@gmail.com>
2019-11-28 18:44:49 -0800

    First commit of lab 9

---
+++ cmake-build-debug/CMakeFiles/abstractfactory.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abstractfactory.dir/main.cpp.o"
+  "abstractfactory.exe"
+  "abstractfactory.pdb"
+  "libabstractfactory.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abstractfactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
